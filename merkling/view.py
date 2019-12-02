66f7897e2b86949797d796b9a2b28e0a146ed1cd
protolambda <proto@protolambda.com>
2019-12-02 01:33:12 +0100

    fix viewhook in getter, fix type-base/type-def separation, implement containers

--- merkling/view.py
+++ merkling/view.py
@@ -1,5 +1,5 @@
-from typing import Callable, NewType, Optional
-from merkling.tree import Link, Node, Root, RootNode, subtree_fill_to_length, to_gindex, zero_node, merkle_hash
+from typing import Callable, NewType, Optional, Sequence, NamedTuple
+from merkling.tree import Link, Node, Root, RootNode, subtree_fill_to_length, subtree_fill_to_contents, to_gindex, zero_node, merkle_hash
 
 
 # Get the depth required for a given element count
@@ -9,6 +9,10 @@ def get_depth(elem_count: int) -> int:
 
 
 class TypeDef(type):
+    pass
+
+
+class TypeBase(type, metaclass=TypeDef):
     def default_node(self) -> Node:
         raise NotImplementedError
 
@@ -17,10 +21,6 @@ class TypeDef(type):
 
     def default(self, hook: Optional["ViewHook"]) -> "View":
         return self.view_from_backing(self.default_node(), hook)
-
-
-class TypeBase(type, metaclass=TypeDef):
-    pass
 
 
 class View(object, metaclass=TypeBase):
@@ -33,13 +33,12 @@ class View(object, metaclass=TypeBase):
 
 
 class BackedType(TypeBase, metaclass=TypeDef):
-
     def view_from_backing(cls, node: Node, hook: Optional["ViewHook"]) -> "View":
         return cls(backing=node, hook=hook)
 
 
 class BackedView(View, metaclass=BackedType):
-    hook: Optional["ViewHook"]
+    _hook: Optional["ViewHook"]
     _backing: Node
 
     def __init__(self, *args, **kw):
@@ -47,7 +46,7 @@ class BackedView(View, metaclass=BackedType):
             self._backing = kw.pop("backing")
         else:
             self._backing = self.__class__.default_node()
-        self.hook = kw.pop("hook", None)
+        self._hook = kw.pop("hook", None)
         super().__init__(*args, **kw)
 
     def get_backing(self) -> Node:
@@ -56,8 +55,8 @@ class BackedView(View, metaclass=BackedType):
     def set_backing(self, value):
         self._backing = value
         # Propagate up the change if the view is hooked to a super view
-        if self.hook is not None:
-            self.hook(self)
+        if self._hook is not None:
+            self._hook(self)
 
 
 ViewHook = NewType("ViewHook", Callable[[View], None])
@@ -72,7 +71,7 @@ class SubtreeView(BackedView, metaclass=SubtreeType):
 
     def get(self, i: int) -> View:
         return self.__class__.__class__.view_from_backing(
-            self.get_backing().getter(to_gindex(i, self.__class__.depth())), self.hook)
+            self.get_backing().getter(to_gindex(i, self.__class__.depth())), lambda v: self.set(i, v))
 
     def set(self, i: int, v: View) -> None:
         setter_link: Link = self.get_backing().setter(to_gindex(i, self.__class__.depth()))
@@ -80,6 +79,10 @@ class SubtreeView(BackedView, metaclass=SubtreeType):
 
 
 class BasicTypeDef(TypeDef):
+    pass
+
+
+class BasicTypeBase(TypeBase, metaclass=BasicTypeDef):
     def default_node(cls) -> Node:
         return zero_node(0)
 
@@ -101,10 +104,6 @@ class BasicTypeDef(TypeDef):
         return cls.from_bytes(node.root[i*size:(i+1)*size], byteorder='little')
 
 
-class BasicTypeBase(TypeBase, metaclass=BasicTypeDef):
-    pass
-
-
 class BasicView(View, metaclass=BasicTypeBase):
     def backing_from_base(self, base: RootNode, i: int) -> RootNode:
         raise NotImplementedError
@@ -123,7 +122,7 @@ class VectorType(SubtreeType):
     def depth(cls) -> int:
         return get_depth(cls.length())
 
-    def element_type(cls) -> TypeDef:
+    def element_type(cls) -> TypeBase:
         raise NotImplementedError
 
     def length(cls) -> int:
@@ -137,7 +136,7 @@ class VectorType(SubtreeType):
         (element_type, length) = params
 
         class SpecialVectorType(VectorType):
-            def element_type(cls) -> TypeDef:
+            def element_type(cls) -> TypeBase:
                 return element_type
 
             def length(cls) -> int:
@@ -171,7 +170,63 @@ class Uint64View(int, BasicView, metaclass=Uint64Type):
     pass
 
 
-SimpleVec = Vector[Uint64Type, 512]
+class Fields(NamedTuple):
+    keys: Sequence[str]
+    types: Sequence[TypeBase]
+
+
+class ContainerType(SubtreeType):
+    def depth(cls) -> int:
+        return get_depth(len(cls.fields().keys))
+
+    def fields(cls) -> Fields:
+        raise NotImplementedError
+
+    def default_node(cls) -> Node:
+        print(cls.fields().types)
+        return subtree_fill_to_contents([field.default_node() for field in cls.fields().types], cls.depth())
+
+
+class Container(SubtreeView, metaclass=ContainerType):
+
+    def __init__(self, *args, **kw):
+        annot = self.__class__.__annotations__
+        self.__class__._fields = Fields(keys=list(annot.keys()), types=[v for v in annot.values()])
+        super().__init__(*args, **kw)
+
+    @classmethod
+    def fields(cls) -> Fields:
+        return cls._fields
+
+    def __getattribute__(self, item):
+        if item.startswith('_'):
+            return super().__getattribute__(item)
+        else:
+            i = self.__class__._fields.keys.index(item)
+            return super().get(i)
+
+    def __setattr__(self, key, value):
+        if key.startswith('_'):
+            super().__setattr__(key, value)
+        else:
+            i = self.__class__._fields.keys.index(key)
+            super().set(i, value)
+
+
+class TestCon(Container):
+    foo: Uint64View
+    bar: Vector[Uint64View, 4]
+
+
+print(Uint64View.default_node())
+print(Vector[Uint64View, 4].default_node())
+
+a = TestCon
+print(a)
+b = a()
+print(b)
+
+SimpleVec = Vector[Uint64View, 512]
 print(SimpleVec)
 data: Vector = SimpleVec()
 print(data)
