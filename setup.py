ccf152bd70ed7af2dd225d1e0ca09f74f5f8cd00
protolambda <proto@protolambda.com>
2020-01-07 01:11:14 +0100

    extend setup

--- setup.py
+++ setup.py
@@ -1,17 +1,36 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='remerkleable',
-    description='Typed mutable SSZ views over cached and immutable binary merkle trees',
-    author='protolambda',
-    url='https://github.com/protolambda/remerkleable',
-    python_requires='>=3.8',
+    name="remerkleable",
+    description="Typed mutable SSZ views over cached and immutable binary merkle trees",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="protolambda",
+    author_email="proto+pip@protolambda.com",
+    url="https://github.com/protolambda/remerkleable",
+    python_requires=">=3.8, <4",
+    license="MIT",
     packages=find_packages(),
+    py_modules=["remerkleable"],
     tests_require=[],
     extras_require={
-        'testing': ["pytest"],
+        "testing": ["pytest"],
         "linting": ["flake8"]
     },
     install_requires=[],
-    keywords=['merkle', 'merkleize', 'merkle-tree', 'merkle-trie', 'trie', 'ssz', 'hash-tree-root', 'eth2'],
+    include_package_data=True,
+    keywords=["merkle", "merkleize", "merkle-tree", "merkle-trie", "trie", "ssz", "hash-tree-root", "eth2"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+    ],
 )
