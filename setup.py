469907b68a46862083e4f40361ff788a63d01b5d
protolambda <proto@protolambda.com>
2020-01-16 04:41:02 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="remerkleable",
     description="Typed mutable SSZ views over cached and immutable binary merkle trees",
-    version="0.1.1",
+    version="0.1.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="protolambda",
